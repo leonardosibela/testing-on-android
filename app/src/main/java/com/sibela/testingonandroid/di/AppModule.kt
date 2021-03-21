ad6d44ef62b529eddeb0407d9693054684bae1a1
leonardosibela <leonardo_sibela@hotmail.com>
2021-03-21 15:24:15 -0300

    Adding AppModule

---
+++ app/src/main/java/com/sibela/testingonandroid/di/AppModule.kt
@@ -0,0 +1,43 @@
+package com.sibela.testingonandroid.di
+
+import android.content.Context
+import androidx.room.Room
+import com.sibela.testingonandroid.data.local.ShoppingItemDatabase
+import com.sibela.testingonandroid.data.remote.PixabayAPI
+import com.sibela.testingonandroid.other.Constants.BASE_URL
+import com.sibela.testingonandroid.other.Constants.DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideShoppingItemDatabase(@ApplicationContext context: Context) =
+        Room.databaseBuilder(context, ShoppingItemDatabase::class.java, DATABASE_NAME).build()
+
+
+    @Provides
+    @Singleton
+    fun provideShoppingDao(database: ShoppingItemDatabase) =
+        database.shoppingDao()
+
+    @Provides
+    @Singleton
+    fun providePixabayApi() = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(BASE_URL)
+        .build()
+        .create(PixabayAPI::class.java)
+
+
+
+}
---
+++ app/src/main/java/com/sibela/testingonandroid/other/Constants.kt
@@ -0,0 +1,8 @@
+package com.sibela.testingonandroid.other
+
+object Constants {
+
+    const val DATABASE_NAME = "shopping_db"
+    const val BASE_URL = "https://pixabay.com"
+
+}
