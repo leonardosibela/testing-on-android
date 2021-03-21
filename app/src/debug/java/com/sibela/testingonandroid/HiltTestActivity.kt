9c28e43a35e1d548f5e8df0c54ab2e9af8d52285
leonardosibela <leonardo_sibela@hotmail.com>
2021-03-21 18:15:21 -0300

    Setup for testing fragments with Dagger-Hilt

---
+++ app/src/debug/java/com/sibela/testingonandroid/HiltTestActivity.kt
@@ -0,0 +1,7 @@
+package com.sibela.testingonandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity()
