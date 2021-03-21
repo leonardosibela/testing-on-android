16e4d0a0bb9c2b93beeab77d68c0af4d2b7793e4
leonardosibela <leonardo_sibela@hotmail.com>
2021-03-21 17:54:53 -0300

    Testing with Dagger-Hilt

--- app/src/androidTest/java/com/sibela/testingonandroid/data/local/ShoppingDaoTest.kt
+++ app/src/androidTest/java/com/sibela/testingonandroid/data/local/ShoppingDaoTest.kt
@@ -1,37 +1,39 @@
 package com.sibela.testingonandroid.data.local
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.room.Room
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
 import com.google.common.truth.Truth.assertThat
 import com.sibela.testingonandroid.getOrAwaitValue
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
+import javax.inject.Inject
+import javax.inject.Named
 
 @SmallTest
+@HiltAndroidTest
 @ExperimentalCoroutinesApi
-@RunWith(AndroidJUnit4::class)
 class ShoppingDaoTest {
 
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
 
-    private lateinit var database: ShoppingItemDatabase
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    @Named("test_db")
+    lateinit var database: ShoppingItemDatabase
     private lateinit var shoppingDao: ShoppingDao
 
     @Before
     fun setup() {
-        database = Room.inMemoryDatabaseBuilder(
-            ApplicationProvider.getApplicationContext(),
-            ShoppingItemDatabase::class.java
-        ).allowMainThreadQueries().build()
+        hiltRule.inject()
         shoppingDao = database.shoppingDao()
     }
 
