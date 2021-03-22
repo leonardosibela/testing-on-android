d09504f82249ce43db91a4af29756cc15811ddbc
leonardosibela <leonardo_sibela@hotmail.com>
2021-03-22 10:13:13 -0300

    Test Swipe to Delete Functionality

--- app/src/androidTest/java/com/sibela/testingonandroid/ui/ShoppingFragmentTest.kt
+++ app/src/androidTest/java/com/sibela/testingonandroid/ui/ShoppingFragmentTest.kt
@@ -1,12 +1,19 @@
 package com.sibela.testingonandroid.ui
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.swipeLeft
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.filters.MediumTest
+import com.google.common.truth.Truth.assertThat
 import com.sibela.testingonandroid.R
+import com.sibela.testingonandroid.adapters.ShoppingItemAdapter
+import com.sibela.testingonandroid.data.local.ShoppingItem
+import com.sibela.testingonandroid.getOrAwaitValue
 import com.sibela.testingonandroid.launchFragmentInHiltContainer
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
@@ -16,6 +23,7 @@ import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
+import javax.inject.Inject
 
 @MediumTest
 @HiltAndroidTest
@@ -24,6 +32,12 @@ class ShoppingFragmentTest {
 
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
+
+    @get:Rule
+    var instTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Inject
+    lateinit var testFragmentFactory: TestShoppingFragmentFactory
 
     @Before
     fun setup() {
@@ -41,5 +55,26 @@ class ShoppingFragmentTest {
 
         verify(navController)
             .navigate(ShoppingFragmentDirections.actionShoppingFragmentToAddShoppingItemFragment())
+    }
+
+    @Test
+    fun swipeShoppingItem_deleteItemInDb() {
+        val shoppingItem = ShoppingItem(1, "test", 1, 1f, "test")
+        var testViewModel: ShoppingViewModel? = null
+        launchFragmentInHiltContainer<ShoppingFragment>(
+            fragmentFactory = testFragmentFactory
+        ) {
+            testViewModel = viewModel
+            viewModel?.insertShoppingItemIntoDb(shoppingItem)
+        }
+
+        onView(withId(R.id.rvShoppingItems)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<ShoppingItemAdapter.ShoppingItemViewHolder>(
+                0,
+                swipeLeft()
+            )
+        )
+
+        assertThat(testViewModel?.shoppingItems?.getOrAwaitValue()).isEmpty()
     }
 }
--- app/src/androidTest/java/com/sibela/testingonandroid/ui/TestShoppingFragmentFactory.kt
+++ app/src/androidTest/java/com/sibela/testingonandroid/ui/TestShoppingFragmentFactory.kt
@@ -5,10 +5,10 @@ import androidx.fragment.app.FragmentFactory
 import com.bumptech.glide.RequestManager
 import com.sibela.testingonandroid.adapters.ImageAdapter
 import com.sibela.testingonandroid.adapters.ShoppingItemAdapter
-import com.sibela.testingonandroid.repositories.ShoppingRepository
+import com.sibela.testingonandroid.repositories.FakeShoppingRepositoryAndroidTest
 import javax.inject.Inject
 
-class ShoppingFragmentFactory @Inject constructor(
+class TestShoppingFragmentFactory @Inject constructor(
     private val imageAdapter: ImageAdapter,
     private val glide: RequestManager,
     private val shoppingItemAdapter: ShoppingItemAdapter
@@ -18,7 +18,10 @@ class ShoppingFragmentFactory @Inject constructor(
         return when (className) {
             ImagePickFragment::class.java.name -> ImagePickFragment(imageAdapter)
             AddShoppingItemFragment::class.java.name -> AddShoppingItemFragment(glide)
-            ShoppingFragment::class.java.name -> ShoppingFragment(shoppingItemAdapter)
+            ShoppingFragment::class.java.name -> ShoppingFragment(
+                shoppingItemAdapter,
+                ShoppingViewModel(FakeShoppingRepositoryAndroidTest())
+            )
             else -> return super.instantiate(classLoader, className)
         }
     }
