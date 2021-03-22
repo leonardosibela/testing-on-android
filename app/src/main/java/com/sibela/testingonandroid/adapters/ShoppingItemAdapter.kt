586539b00f712d903f5780e01d0d859525070fee
leonardosibela <leonardo_sibela@hotmail.com>
2021-03-22 09:51:35 -0300

    Implementing ShoppingFragment

---
+++ app/src/main/java/com/sibela/testingonandroid/adapters/ShoppingItemAdapter.kt
@@ -0,0 +1,59 @@
+package com.sibela.testingonandroid.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.RequestManager
+import com.sibela.testingonandroid.R
+import com.sibela.testingonandroid.data.local.ShoppingItem
+import kotlinx.android.synthetic.main.item_image.view.ivShoppingImage
+import kotlinx.android.synthetic.main.item_shopping.view.*
+import javax.inject.Inject
+
+class ShoppingItemAdapter @Inject constructor(
+    private val glide: RequestManager
+) : RecyclerView.Adapter<ShoppingItemAdapter.ShoppingItemViewHolder>() {
+
+    private val diffCallback = object : DiffUtil.ItemCallback<ShoppingItem>() {
+        override fun areItemsTheSame(oldItem: ShoppingItem, newItem: ShoppingItem): Boolean {
+            return newItem.id == oldItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: ShoppingItem, newItem: ShoppingItem): Boolean {
+            return newItem == oldItem
+        }
+
+    }
+
+    private val differ = AsyncListDiffer(this, diffCallback)
+
+    var shoppingItems: List<ShoppingItem>
+        get() = differ.currentList
+        set(value) = differ.submitList(value)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShoppingItemViewHolder {
+        return ShoppingItemViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_shopping, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: ShoppingItemViewHolder, position: Int) {
+        val shoppingItem = shoppingItems[position]
+        holder.itemView.apply {
+            glide.load(shoppingItem.imageUrl).into(ivShoppingImage)
+            tvName.text = shoppingItem.name
+            val amountText = "${shoppingItem.amount}x"
+            tvShoppingItemAmount.text = amountText
+            val priceText = "${shoppingItem.price}€"
+            tvShoppingItemPrice.text = priceText
+        }
+    }
+
+    override fun getItemCount() = shoppingItems.size
+
+    class ShoppingItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+}
--- app/src/main/java/com/sibela/testingonandroid/ui/ShoppingFragment.kt
+++ app/src/main/java/com/sibela/testingonandroid/ui/ShoppingFragment.kt
@@ -3,23 +3,75 @@ package com.sibela.testingonandroid.ui
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.ItemTouchHelper.LEFT
+import androidx.recyclerview.widget.ItemTouchHelper.RIGHT
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.sibela.testingonandroid.R
+import com.sibela.testingonandroid.adapters.ShoppingItemAdapter
 import kotlinx.android.synthetic.main.fragment_shopping.*
+import javax.inject.Inject
 
-class ShoppingFragment : Fragment(R.layout.fragment_shopping) {
+class ShoppingFragment @Inject constructor(
+    val shoppingItemAdapter: ShoppingItemAdapter,
+    var viewModel: ShoppingViewModel? = null
+) : Fragment(R.layout.fragment_shopping) {
 
-    private lateinit var viewModel: ShoppingViewModel
+    private val itemTouchCallback = object : ItemTouchHelper.SimpleCallback(
+        0, LEFT or RIGHT
+    ) {
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ) = true
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            val position = viewHolder.layoutPosition
+            val item = shoppingItemAdapter.shoppingItems[position]
+            viewModel?.deleteShoppingItem(item)
+            Snackbar.make(requireView(), "Successfully deleted item", Snackbar.LENGTH_LONG).apply {
+                setAction("Undo") {
+                    viewModel?.insertShoppingItemIntoDb(item)
+                }
+            }
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        viewModel = ViewModelProvider(requireActivity()).get(ShoppingViewModel::class.java)
+        viewModel = viewModel ?: ViewModelProvider(requireActivity()).get(ShoppingViewModel::class.java)
+        subscribeToObservers()
+        setupRecyclerView()
 
         fabAddShoppingItem.setOnClickListener {
             findNavController()
                 .navigate(ShoppingFragmentDirections.actionShoppingFragmentToAddShoppingItemFragment())
         }
+    }
+
+    private fun setupRecyclerView() {
+        rvShoppingItems.apply {
+            adapter = shoppingItemAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+            ItemTouchHelper(itemTouchCallback).attachToRecyclerView(this)
+        }
+    }
+
+    private fun subscribeToObservers() {
+        viewModel?.shoppingItems?.observe(viewLifecycleOwner, Observer {
+            shoppingItemAdapter.shoppingItems = it
+        })
+
+        viewModel?.totalPrice?.observe(viewLifecycleOwner, Observer {
+            val price = it ?: 0f
+            val priceText = "Total price $price€"
+            tvShoppingItemPrice.text = priceText
+        })
     }
 }
